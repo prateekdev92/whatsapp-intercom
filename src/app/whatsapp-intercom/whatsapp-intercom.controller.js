b8c321b8d2716844d07a6f7006799149a47cab54
Prateek Jaiswal <prateek.jaiswal@shadowfax.in>
2018-12-04 21:11:44 +0530

    delayed search

--- src/app/whatsapp-intercom/whatsapp-intercom.controller.js
+++ src/app/whatsapp-intercom/whatsapp-intercom.controller.js
@@ -3,14 +3,23 @@ export class CommController {
     'ngInject';
 
     var vm = this;
-    vm.showFullViewData = null;
-    vm.showFullView = showFullView;
-    vm.chat_push_message = chat_push_message;
-    vm.chat_user_submitted = chat_user_submitted
     var webSocketUrl = "wss://echo.websocket.org/";
+    vm.showFullViewData = null;
     vm.websocket = null;
 
-    function ConnectWebSocket() {
+    vm.showFullView = showFullView;
+    vm.chat_user_submitted = chat_user_submitted;
+    vm.delayed_search = delayed_search;
+
+    var pingBack = (evt) => {
+      setTimeout(function(){
+        chat_push_message("Reply: " + evt.data, vm.showFullViewData, vm.showFullViewData.id, function() {
+        vm.showFullView(vm.showFullViewData);
+      });
+      },500)
+    }
+
+    var ConnectWebSocket = () => {
       vm.websocket = new WebSocket(webSocketUrl);
       vm.websocket.onopen = function(evt) {
         console.log("liftOff");
@@ -22,15 +31,11 @@ export class CommController {
         pingBack(evt)
       };
       vm.websocket.onerror = function(evt) {
-        throw new Error("Out of Trajectory")
+        throw new Error("Out of Trajectory");
+        vm.AppOnline = !vm.AppOnline;
       };
     }
 
-    function pingBack(evt) {
-      chat_push_message("Reply: " + evt.data, vm.showFullViewData, vm.showFullViewData.id, function() {
-        vm.showFullView(vm.showFullViewData);
-      });
-    }
 
     function showFullView(data) {
       vm.showFullViewData = data;
@@ -41,15 +46,12 @@ export class CommController {
         $scope.$apply();
         objDiv.scrollTop = objDiv.scrollHeight;
       }
+      vm.search = null;
+      init();
     }
 
-    function chat_user_submitted(msg, data, by) {
-      chat_push_message(msg, data, by, function() {
-        vm.websocket.send(msg);
-      });
-    }
 
-    function chat_push_message(msg, data, by, cb) {
+    var chat_push_message = (msg, data, by, cb) => {
 
       var id = data.id;
       if (localStorage.getItem(id)) {
@@ -57,6 +59,7 @@ export class CommController {
         msgs = JSON.parse(msgs);
         msgs.push({ "message": msg, "time": Date.now(), "by": by });
         localStorage.setItem(id, JSON.stringify(msgs));
+        vm.showFullView(vm.showFullViewData);
       } else {
         localStorage.setItem(id, JSON.stringify([{ "message": msg, "time": Date.now(), "by": "me" }]))
       }
@@ -66,7 +69,64 @@ export class CommController {
 
     }
 
-    vm.friend_list = commService.fetch_data();
+    function chat_user_submitted(msg, data, by) {
+      chat_push_message(msg, data, by, function() {
+        vm.websocket.send(msg);
+      });
+    }
+
+    function debounce(fn, delay) {
+
+      let setTimeOutRes = null;
+
+      return function() {
+
+        let context = this;
+        let args = arguments;
+
+        clearTimeout(setTimeOutRes);
+        setTimeOutRes = setTimeout(function() {
+          fn.apply(context, args);
+        }, delay);
+
+      }
+
+    }
+
+    var search_friends = (name) => {
+
+      var arr = [];
+
+      vm.friend_list.forEach(function(i) {
+        let fname = (i.fname);
+        fname = fname.toLowerCase();
+        if (fname.indexOf(name) != -1) {
+          arr.push(i);
+        }
+      });
+
+      if (vm.friend_list.length != arr.length) {
+        vm.friend_list = arr;
+      }
+
+    }
+
+    function delayed_search(name, event) {
+
+      if (name == '' || event.which === 8) {
+        init();
+      } else if (name.length > 1) {
+        (debounce(search_friends, 300)(name));
+      }
+
+    }
+
+    var init = () => {
+      vm.AppOnline = navigator.onLine;
+      vm.friend_list = commService.fetch_data();
+    }
+
+    init();
     ConnectWebSocket();
 
   }
